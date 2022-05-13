cab4abbbae87dc2ac98194ac848f72cb72591074
JuniorAndre <65295725+JuniorAndre@users.noreply.github.com>
2022-05-13 01:33:31 -0300

    update

--- Práticas/Repetição e Arrays em java/Arrays/consoantes.java
+++ Práticas/Repetição e Arrays em java/Arrays/consoantes.java
@@ -7,15 +7,15 @@ public class consoantes{
         String[] consoantes = new String[6];
         int qtdConsoantes = 0;
 
-        for(int i = 0; i < array.length; i++){
+        for(int i = 0; i < consoantes.length; i++){
             System.out.println("Letra: ");
-            String letra = next();
+            String letra = scan.next();
 
-            if(!( letra.equalsIgnoreCase("a")) |
-                ( letra.equalsIgnoreCase("e")) |
-                ( letra.equalsIgnoreCase("i")) |
-                ( letra.equalsIgnoreCase("o")) |
-                ( letra.equalsIgnoreCase ("u")) ){
+            if(!( letra.equalsIgnoreCase("a") |
+                  letra.equalsIgnoreCase("e") |
+                  letra.equalsIgnoreCase("i") |
+                  letra.equalsIgnoreCase("o") |
+                  letra.equalsIgnoreCase("u"))){
                     consoantes[i] = letra;
                     qtdConsoantes++;
                 }
