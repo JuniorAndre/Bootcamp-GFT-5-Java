aac5c26e78385564b179d423973ab5a8b82906d5
JuniorAndre <65295725+JuniorAndre@users.noreply.github.com>
2022-05-13 00:29:48 -0300

    create file

---
+++ Práticas/Repetição e Arrays em java/Arrays/ordemInversa.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ordemInversa{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        int array = [];
+
+        System.out.println("Digite 5 números para o array: ");
+        array[0] = scan.nextInt();
+        array[1] = scan.nextInt();
+        array[2] = scan.nextInt();
+        array[3] = scan.nextInt();
+        array[4] = scan.nextInt();
+
+
+        System.out.println("O inverso do array é: " + array[]);
+    }
+}
