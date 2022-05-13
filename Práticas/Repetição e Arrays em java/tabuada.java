522ea1feb0fb8a338de8e66b9bf46007b46a1100
JuniorAndre <65295725+JuniorAndre@users.noreply.github.com>
2022-05-12 23:39:58 -0300

    update

--- Práticas/Repetição e Arrays em java/tabuada.java
+++ Práticas/Repetição e Arrays em java/tabuada.java
@@ -10,7 +10,7 @@ public class tabuada{
         System.out.println("Tabuada de " + num + ":");
 
 
-        for(int i=0; i<10; i++){
+        for(int i=1; i<=10; i++){
             System.out.println(num + " X " + i + " = " + num*i);
         }
     }
