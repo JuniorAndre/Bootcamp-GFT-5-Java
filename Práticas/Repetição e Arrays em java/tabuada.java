8ec9cc8301359987fc2aa251e602853276868ba5
JuniorAndre <65295725+JuniorAndre@users.noreply.github.com>
2022-05-12 23:37:12 -0300

    create file

---
+++ Práticas/Repetição e Arrays em java/tabuada.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class tabuada{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        int num;
+        System.out.println("Digite de 1 a 10 para ver a tabuada desse número: ");
+        num = scan.nextInt();
+        System.out.println("Tabuada de " + num + ":");
+
+
+        for(int i=0; i<10; i++){
+            System.out.println(num + " X " + i + " = " + num*i);
+        }
+    }
+}
