dc38ec8d1f4fb60d685fe50119a16335600ac7c5
JuniorAndre <65295725+JuniorAndre@users.noreply.github.com>
2022-05-25 02:37:08 -0300

    create files

---
+++ Desafios/bancoEmJavaComOO/conta.java
@@ -0,0 +1,16 @@
+public class conta{
+    private int agencia;
+    private int numero;
+    private double saldo;
+
+    public void sacar(){
+
+    }
+    public void depositar(){
+
+    }
+    public void transferir(){
+
+    }
+
+}
